96524711d32af4f8b3cc02395e59aa5d5433b9d9
barbosso <58640181+barbosso@users.noreply.github.com>
2022-08-08 18:13:50 +0300

    Add files via upload

---
+++ timershutdown.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+shutdown -h +$1 > /dev/null 2>&1
+hour=0
+min=$1
+sec=0
+	while [ $hour -ge 0 ]; do
+    	while [ $min -ge 0 ]; do
+        	while [ $sec -ge 0 ]; do
+            	echo -ne "$hour:$min:$sec\033[0K\r"
+                let "sec=sec-1"
+                sleep 1
+            done
+            sec=59
+            let "min=min-1"
+        done
+        min=59
+        let "hour=hour-1"
+    done
